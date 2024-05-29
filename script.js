65a88c3c82d9e12e92a22e2fdefbe716f3cff9e9
realnilesh <148353585+realnilesh@users.noreply.github.com>
2024-05-29 15:31:58 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log("javascipt is running");
+
+let boxes=document.querySelector(".container").children
+
+function getRandomColor()
+{
+    let r=Math.random()*255;
+    let g=Math.random()*255;
+    let b=Math.random()*255;
+    return `rgb(${r},${g},${b})`
+}
+
+Array.from(boxes).forEach(e=>
+    {
+        e.style.backgroundColor=getRandomColor()
+        e.style.color=getRandomColor()
+    }
+)
